d19b93fd914b1e74ab8bd72b62558b0e80d66407
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-08-12 20:05:10 -0300

    reply card

--- src/app/components/reply-card/reply-card.component.ts
+++ src/app/components/reply-card/reply-card.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+interface Reply {
+  id: number;
+  date: string;
+  author: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-reply-card',
@@ -6,5 +14,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./reply-card.component.scss']
 })
 export class ReplyCardComponent {
+  @Input()
+  reply!: Reply;
+  datePipe = new DatePipe('en-US');
+  originalDate: string = "";
+  formattedDate = this.datePipe.transform(this.originalDate, 'MMM. d, yyyy, h:mm a'); // This will hold the formatted date
 
+  ngOnInit(): void {
+    this.originalDate =  this.reply.date;
+    this.formattedDate = this.datePipe.transform(this.originalDate, 'MMM. d, yyyy, h:mm a');
+  }
 }
