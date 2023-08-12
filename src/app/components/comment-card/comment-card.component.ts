92229209e4a6f8dc24ff5c5cbb0fd129ba029a12
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-08-12 18:39:30 -0300

    comment card styled

--- src/app/components/comment-card/comment-card.component.ts
+++ src/app/components/comment-card/comment-card.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+interface CommentData {
+  id: number;
+  date: string;
+  author: string;
+  comment: string;
+  replies: Reply[]|[];
+}
+interface Reply {
+  id: number;
+  date: string;
+  author: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-comment-card',
@@ -6,5 +21,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./comment-card.component.scss']
 })
 export class CommentCardComponent {
+  @Input()
+  comment!: CommentData;
+  datePipe = new DatePipe('en-US');
+  originalDate: string = "";
+  formattedDate = this.datePipe.transform(this.originalDate, 'MMM. d, yyyy, h:mm a'); // This will hold the formatted date
+
+  ngOnInit(): void {
+    this.originalDate =  this.comment.date
+  }
 
 }
--- src/app/components/right-sidebar/right-sidebar.component.ts
+++ src/app/components/right-sidebar/right-sidebar.component.ts
@@ -14,7 +14,7 @@ export class RightSidebarComponent {
       'max-width': this.openDrawer ? '367.25px' : '0',
       'width': this.openDrawer ? '367.25px' : '0',
       'left': this.openDrawer ? '0' : '-367.25px',
-      'transition': 'max-width 0.3s, width 0.3s, left 0.3s',
+      'transition': 'max-width 0.2s, width 0.2s, left 0.2s',
     };
   }
 
